b44643f9e8d2e65e42e19b2abefb09f0270392b1
Harsha <harshapu4@gmail.com>
2025-11-02 23:01:52 +0530

    fix tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{jsx,tsx}", "./*.html"],
-    theme: {
-        extend: {
-            colors: {
-                dark: "#212429",
-                darkHover: "#3D404A",
-                light: "#f5f5f5",
-                primary: "#39E079",
-                danger: "#ef4444",
-            },
-            fontFamily: {
-                poppins: ["Poppins", "sans-serif"],
-            },
-            animation: {
-                "up-down": "up-down 2s ease-in-out infinite alternate",
-            },
-        },
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // everything in src
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{css,scss}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#212429",
+        darkHover: "#3D404A",
+        light: "#f5f5f5",
+        primary: "#39E079",
+        danger: "#ef4444",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      animation: {
+        "up-down": "up-down 2s ease-in-out infinite alternate",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }
